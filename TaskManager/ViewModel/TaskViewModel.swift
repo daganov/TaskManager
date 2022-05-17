fc787f840bbb72501d2292876b9ed7f130a7b9d6
Denis Aganov <den.aganov@yandex.ru>
2022-05-17 20:59:10 +0300

    Add fetchCurrentWeek in TaskViewModel

--- TaskManager/ViewModel/TaskViewModel.swift
+++ TaskManager/ViewModel/TaskViewModel.swift
@@ -13,4 +13,27 @@ class TaskViewModel: ObservableObject {
         Task(taskTitle: "Next Project", taskDescription: "Discuss next project", taskDate: .init(timeIntervalSince1970: 1641677897)),
         Task(taskTitle: "App Proposal", taskDescription: "Meet Client", taskDate: .init(timeIntervalSince1970: 1641677897)),
     ]
+    
+    // MARK: Current Week Days
+    @Published var currentWeek = [Date]()
+    
+    init() {
+        fetchCurrentWeek()
+    }
+    
+    func fetchCurrentWeek() {
+        let today = Date()
+        let calendar = Calendar.current
+        let week = calendar.dateInterval(of: .weekOfMonth, for: today)
+        
+        guard let firstWeekDay = week?.start else {
+            return
+        }
+        
+        (1...7).forEach { day in
+            if let weekday = calendar.date(byAdding: .day, value: day, to: firstWeekDay) {
+                currentWeek.append(weekday)
+            }
+        }
+    }
 }
